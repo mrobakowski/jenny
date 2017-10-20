c495dfbb910f95ab169c8067dfda2efd31832024
mrobakowski <mikolaj.rob@gmail.com>
2017-10-21 00:06:36 +0200

    make things work on windows AND on linux (I hope)

--- kotlin-example/src/main/kotlin/com/mrobakowski/jenny/HelloWorld.kt
+++ kotlin-example/src/main/kotlin/com/mrobakowski/jenny/HelloWorld.kt
@@ -2,9 +2,18 @@ package com.mrobakowski.jenny
 
 object HelloWorld {
     init {
-        System.load("${System.getProperty("user.dir")}/target/debug/examples/hello-world.dll")
+        System.load("${System.getProperty("user.dir")}/target/debug/examples/${platformSpecificLibName("hello-world")}")
     }
 
     external fun helloWorld()
     external fun foo(x: Long, y: Float): Double
+}
+
+fun platformSpecificLibName(name: String): String {
+    val platform = System.getProperty("os.name").toLowerCase()
+    return when {
+        "win" in platform -> "$name.dll"
+        "nix" in platform || "nux" in platform -> "lib$name.so"
+        else -> "<unknown platform>"
+    }
 }
